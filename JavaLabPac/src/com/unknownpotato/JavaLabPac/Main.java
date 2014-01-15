c68a084279e2cc0b370fba43a0c2d7baa94d7a5b
Victor Bankowski <victor.bankowski@cs.helsinki.fi>
2014-01-15 13:24:15 +0200

    added a eclipse project that should build correctly

---
+++ JavaLabPac/src/com/unknownpotato/JavaLabPac/Main.java
@@ -0,0 +1,13 @@
+package com.unknownpotato.JavaLabPac;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello elcipse c:");
+		new LwjglApplication(new Game(), "Game", 480, 320, false);
+	}
+
+}
