081ac8f879b94c621cb0d86b6ebae3d7e049d638
Victor Bankowski <victor.bankowski@cs.helsinki.fi>
2014-01-30 18:52:39 +0200

    walltest has one test.

--- JavaLabPac/src/test/java/com/unknownpotato/javalabpac/entities/WallTest.java
+++ JavaLabPac/src/test/java/com/unknownpotato/javalabpac/entities/WallTest.java
@@ -2,13 +2,36 @@ package com.unknownpotato.javalabpac.entities;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.GdxNativesLoader;
+
 public class WallTest {
+	
+	@BeforeClass
+	public static void setUpBeforeClass(){
+		GdxNativesLoader.load();
+	}
+
+	private World world;
+	private Wall wall;
+	
+
+	@Before
+	public void setUp() throws Exception {
+//		GdxNativesLoader.load();
+		this.world = new World(new Vector2(),true);
+		this.wall = new Wall(new Vector2() , this.world, null);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void bodyIsinthecorrectworldtest() {
+		World world2 = this.wall.getBody().getWorld();
+		assertTrue(world2 == this.world);
 	}
 
 }
