9b878501c9d9cef783859cba3bf808edfb00c43d
Victor Bankowski <victor.bankowski@cs.helsinki.fi>
2014-02-14 11:09:28 +0200

    added gdxnatovesloader.load to main.

--- JavaLabPac/src/main/java/com/unknownpotato/javalabpac/Main.java
+++ JavaLabPac/src/main/java/com/unknownpotato/javalabpac/Main.java
@@ -1,12 +1,14 @@
 package com.unknownpotato.javalabpac;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.utils.GdxNativesLoader;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello elcipse c:");
+		GdxNativesLoader.load();
 		new LwjglApplication(new Game(), "Game", 800, 600, false);
 	}
 
